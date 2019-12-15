895263ce01a9990c403669b8ba7655a7428faa4c
Artem Kazaryan <kazarynarm@mail.ru>
2019-12-15 17:34:09 +0300

    feat: add menu

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,67 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/app.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'main.js'
+//   },
+
+//   devtool: 'inline-source-map',
+//   devServer: {
+//     contentBase: './dist'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.scss/,
+//         use: [
+//           'style-loader',
+//           'css-loader',
+//           'sass-loader',
+//         ],
+//       },
+//       // {
+//       //   test: /\.(jpg|png|jpeg)$/,
+//       //   exclude: /node_modules/,
+//       //   loader: 'file-loader',
+//       //   options: {
+//       //     // outputPath: './assets/img',
+//       //     publicPath: './assets/img/',
+//       //     name: './assets/img/[name].[ext]',
+//       //   }
+//       // },
+//       {
+//         test: /\.(jpg|png|jpeg)$/,
+//         exclude: /node_modules/,
+//         loader: 'file-loader',
+//         options: {
+//           publicPath: './assets/img/',
+//           name: './assets/img/[name].[ext]',
+//         }
+//       },
+//       {
+//         test: /\.(ttf|eot)$/,
+//         exclude: /node_modules/,
+//         loader: 'file-loader',
+//         options: {
+//           publicPath: './assets/fonts/',
+//           name: './assets/fonts/[name].[ext]',
+//         }
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//       filename: 'index.html',
+//     }),
+
+//   ]
+// }
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -24,14 +88,23 @@ module.exports = {
         ],
       },
       {
+        test: /\.(html)/,
+        loader: 'html-loader',
+        options: {
+          esModule: false
+        }
+      },
+
+      {
         test: /\.(jpg|png|jpeg)$/,
         use: [
           {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/img',
-              publicPath: 'assets/img'
+              publicPath: '/assets/img',
+              outputPath: './assets/img/',
+              name: '[folder]/[name].[ext]',
+              esModule: false
             }
           }
         ]
