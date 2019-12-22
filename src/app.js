fb56fd4a6726494e6385110957fd343f83c8071a
Artem Kazaryan <kazarynarm@mail.ru>
2019-12-22 19:24:46 +0300

    feat: add mobile-version menu

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import './style/main.scss';
+
+const checkbox = document.querySelector('#menu-dropper');
+const items = Array.from(document.querySelectorAll('.nav__item'));
+
+checkbox.addEventListener('change', function (e) {
+  items.forEach((el, index) => {
+    if (checkbox.checked === false) {
+      if (index % 2 === 0) {
+        el.classList.add('animate-right_hide');
+      } else {
+        el.classList.add('animate-left_hide');
+      }
+    }
+  });
+});
