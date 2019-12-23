666c9e5599b75a942fc123e15ecbf9c6df15a4e4
Artem Kazaryan <kazarynarm@mail.ru>
2019-12-23 17:54:19 +0300

    fix: fix menu behavior when screen size > 680px

--- src/app.js
+++ src/app.js
@@ -4,20 +4,33 @@ const checkbox = document.querySelector('#menu-dropper');
 const items = Array.from(document.querySelectorAll('.nav__item'));
 const nav = document.querySelector('.nav');
 
-checkbox.addEventListener('change', function (e) {
+function hideMenu() {
   items.forEach((el, index) => {
+    let className;
     if (checkbox.checked === false) {
       if (index % 2 !== 0) {
-        el.classList.add('animate-right_hide');
+        className = 'animate-right_hide'
       } else {
-        el.classList.add('animate-left_hide');
+        className = 'animate-left_hide'
       }
+
+      el.classList.add(className);
+
       nav.style.display = 'block';
       setTimeout(() => {
         nav.style.display = 'none';
+        el.classList.remove(className);
       }, 400);
     } else {
       nav.style.display = 'block';
     }
   });
+}
+
+checkbox.addEventListener('change', function (e) {
+  if (window.matchMedia('(max-width: 680px)').matches) {
+    hideMenu();
+  } else {
+    checkbox.checked = !checkbox.checked;
+  }
 });
