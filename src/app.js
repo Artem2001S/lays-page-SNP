b3c0d4f6d1fc9f03a0a32c42b9f336b79f3c61c0
Artem Kazaryan <kazarynarm@mail.ru>
2019-12-27 14:46:00 +0300

    feat: add animation to lays-promotion section and hide/show menu during scroll (mobile version)

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const nav = document.querySelector('.nav');
 function hideMenu() {
   items.forEach((el, index) => {
     let className;
+    // if need to hide menu
     if (checkbox.checked === false) {
       if (index % 2 !== 0) {
         className = 'animate-right_hide'
@@ -27,10 +28,58 @@ function hideMenu() {
   });
 }
 
-checkbox.addEventListener('change', function (e) {
+checkbox.addEventListener('change', function () {
   if (window.matchMedia('(max-width: 680px)').matches) {
     hideMenu();
   } else {
     checkbox.checked = !checkbox.checked;
   }
 });
+
+function animateLaysPromotions() {
+  let doneFirst = false;
+  let doneSecond = false;
+
+  const redBlock = document.querySelector('.lays-promotions__item[data-item="red"]');
+  const balloon = document.querySelector('.lays-promotions__item[data-item="balloon"]');
+  const beach = document.querySelector('.lays-promotions__item[data-item="beach"]');
+  const map = document.querySelector('.lays-promotions__item[data-item="map"]');
+
+  const clientBottomFirst = balloon.getBoundingClientRect().top - balloon.getBoundingClientRect().height;
+  const clientBottomSecond = map.getBoundingClientRect().top - map.getBoundingClientRect().height;
+
+  if (clientBottomFirst < 0 && doneFirst === false) {
+    doneFirst = true;
+    redBlock.style.animation = 'show_to-right 1s ease-in-out forwards';
+    balloon.style.animation = 'show_to-left 1.2s ease-in forwards';
+    balloon.style.zIndex = 1;
+  }
+
+  if (clientBottomSecond < 0 && doneSecond === false) {
+    doneSecond = true;
+    beach.style.animation = 'show_to-right 1.2s ease-in forwards';
+    map.style.animation = 'show_to-left 1s ease-in-out forwards';
+  }
+}
+
+animateLaysPromotions();
+
+let prevScrollPosition = window.pageYOffset;
+const menu = document.querySelector('.menu');
+window.addEventListener('scroll', () => {
+  // hide menu
+  if (window.matchMedia('(max-width: 680px)').matches) {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (prevScrollPosition < currentScrollPosition) {
+      menu.style.top = '-80px';
+    } else {
+      menu.style.top = '0';
+    }
+
+    prevScrollPosition = window.pageYOffset;
+    return;
+  }
+  animateLaysPromotions();
+});
+
